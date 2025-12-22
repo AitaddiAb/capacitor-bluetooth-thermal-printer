b1ee192500e18e059a8caa25e11e30ee74939fd1
AitaddiAB <AitaddiAB@gmail.com>
2025-12-22 15:49:38 +0100

    Remove example app files and configurations, including .gitignore, capacitor.config.json, package.json, README.md, Vite config, and all Android and iOS project files.

--- example-app/src/js/example.js
+++
@@ -1,6 +0,0 @@
-import { BtThPrinter } from 'capacitor-bluetooth-thermal-printer';
-
-window.testEcho = () => {
-    const inputValue = document.getElementById("echoInput").value;
-    BtThPrinter.echo({ value: inputValue })
-}
