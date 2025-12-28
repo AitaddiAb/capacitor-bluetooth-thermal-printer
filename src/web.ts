94c2c48389211f1ef866a29c20905a56ecf89200
AitaddiAB <AitaddiAB@gmail.com>
2025-12-28 16:09:03 +0100

    Add pnpm lockfile and implement web plugin methods for Bluetooth printer

--- src/web.ts
+++ src/web.ts
@@ -3,8 +3,15 @@ import { WebPlugin } from '@capacitor/core';
 import type { BtThPrinterPlugin } from './definitions';
 
 export class BtThPrinterWeb extends WebPlugin implements BtThPrinterPlugin {
-  async echo(options: { value: string }): Promise<{ value: string }> {
-    console.log('ECHO', options);
-    return options;
+  async connect(_options: { address: string }): Promise<void> {
+    throw this.unimplemented('connect is not implemented on web');
+  }
+
+  async printImage(_options: { image: string; width: number; height: number; align: number }): Promise<void> {
+    throw this.unimplemented('printImage is not implemented on web');
+  }
+
+  async listBluetoothDevices(): Promise<{ devices: Array<{ name: string; address: string }> }> {
+    throw this.unimplemented('listBluetoothDevices is not implemented on web');
   }
 }
